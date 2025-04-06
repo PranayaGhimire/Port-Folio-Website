eaf25e97dfb1d786d71b84bb1a7516782a98bc44
Pranaya Ghimire <pranayaghimire88083@gmail.com>
2025-04-06 10:35:22 +0545

    Added Hover Effects

--- src/components/DarkModeToggle.jsx
+++ src/components/DarkModeToggle.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import  { useEffect, useState } from "react";
 import "../App.css";
 const DarkModeToggle = () => {
   // State to track the current mode
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,20 +1,35 @@
+import { useState } from "react"
 
 
 const ProjectCard = ({imgLink,projectLink,name,TechStack}) => {
+  const [isOpen,setIsOpen]=useState(false);
   return (
     <div className="">
-        <div className="w-80 h-70 overflow-hidden rounded-t-lg hover:border-6 hover:border-yellow-600">
+        <div
+        onMouseOver={()=>setIsOpen(true)}
+        onMouseOut={()=>setIsOpen(false)}
+        className={`w-80 h-70 overflow-hidden rounded-t-lg hover:border-6 hover:border-yellow-600`}>
         <a href={projectLink}>
         <img src={imgLink} alt="Project Card"
       className="w-full h-full object-cover"
       />
+      
             </a>
+       
         </div>
-    <div className="bg-pink-600 text-center rounded-b-lg text-white">
-    <p className="font-bold">Project Name: {name}</p>
-    <p className="font-semibold">Tech Stack: {TechStack}</p>
-    </div>
-     
+        {/* For Computer and Laptop screens having minimum width 1400px */}
+        {isOpen && (
+                  <div className="bg-pink-600 text-center rounded-b-lg text-white">
+                  <p className="font-bold">Project Name: {name}</p>
+                  <p className="font-semibold">Tech Stack: {TechStack}</p>
+                  </div>
+                   
+            )}
+        <div className="bg-pink-600 text-center rounded-b-lg text-white 2xl:hidden block">
+                  <p className="font-bold">Project Name: {name}</p>
+                  <p className="font-semibold">Tech Stack: {TechStack}</p>
+                  </div>
+    
     </div>
   )
 }
