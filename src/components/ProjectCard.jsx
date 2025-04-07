4d7b8b19e3907cc766cce60b7770fe135b9b3ffb
Pranaya Ghimire <pranayaghimire88083@gmail.com>
2025-04-07 13:18:38 +0545

    Minot Fix

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -8,7 +8,7 @@ const ProjectCard = ({imgLink,projectLink,name,TechStack}) => {
         <div
         onMouseOver={()=>setIsOpen(true)}
         onMouseOut={()=>setIsOpen(false)}
-        className={`w-80 h-70 overflow-hidden rounded-t-lg hover:border-6 hover:border-yellow-600`}>
+        className={`w-80 h-70 overflow-hidden rounded-t-lg hover:border-6 ${isOpen?"border-yellow-600":"border-none"}`}>
         <a href={projectLink}>
         <img src={imgLink} alt="Project Card"
       className="w-full h-full object-cover"
@@ -17,15 +17,8 @@ const ProjectCard = ({imgLink,projectLink,name,TechStack}) => {
             </a>
        
         </div>
-        {/* For Computer and Laptop screens having minimum width 1400px */}
-        {isOpen && (
-                  <div className="bg-pink-600 text-center rounded-b-lg text-white">
-                  <p className="font-bold">Project Name: {name}</p>
-                  <p className="font-semibold">Tech Stack: {TechStack}</p>
-                  </div>
-                   
-            )}
-        <div className="bg-pink-600 text-center rounded-b-lg text-white 2xl:hidden block">
+     
+        <div className="bg-pink-600 text-center rounded-b-lg text-white">
                   <p className="font-bold">Project Name: {name}</p>
                   <p className="font-semibold">Tech Stack: {TechStack}</p>
                   </div>
